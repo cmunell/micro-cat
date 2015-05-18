7dee558179b37d2171ad912bc7bbb979c6b2325b
Bill McDowell <forkunited@gmail.com>
2015-05-18 01:53:05 -0400

    Added document set test

---
+++ src/test/java/edu/cmu/ml/rtw/micro/cat/data/annotation/nlp/DocumentSetNLPFactoryTest.java
@@ -0,0 +1,22 @@
+package edu.cmu.ml.rtw.micro.cat.data.annotation.nlp;
+
+import org.junit.Test;
+
+import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLP;
+import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentSetNLP;
+import edu.cmu.ml.rtw.generic.util.OutputWriter;
+import edu.cmu.ml.rtw.micro.cat.data.CatDataTools;
+import edu.cmu.ml.rtw.micro.cat.util.CatProperties;
+
+public class DocumentSetNLPFactoryTest {
+	@Test
+	public void testDocumentSetNLP() {
+		CatDataTools dataTools = new CatDataTools(new OutputWriter(), new CatProperties());
+		DocumentSetNLP<DocumentNLP> documents = DocumentSetNLPFactory.getDocumentSet(DocumentSetNLPFactory.SetName.HazyFacc1,
+											 dataTools.getProperties(),
+											 dataTools);
+		for (String documentName : documents.getDocumentNames()) {
+			System.out.println(documentName);
+		}									 
+	}
+}
